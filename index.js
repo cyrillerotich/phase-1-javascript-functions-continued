6e28fe9e8128499930aa43bc685c8b01c30d61b5
cyrille <cyrilllerotich@gmail.com>
2023-06-20 15:46:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(Activity = "roller-skate"){
+    return `This Saturday, I want to ${Activity}!`
+}
+function mondayWork(Activity = "go to the office"){
+    return `This Monday, I will ${Activity}.`
+}
+function wrapAdjective(flair = "*"){
+    return function (rotich="special"){
+        return `You are ${flair}${rotich}${flair}!`
+    }
+}
+    
